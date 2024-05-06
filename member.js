c6219cd0e4cd1ca4d54e5ffdb81abb352e7e1fc5
Andy D <8628078+rpigu-i@users.noreply.github.com>
2024-05-06 15:24:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    let memberSkills = [
+        { skill: 'HTML', level: 5 },
+        { skill: 'CSS', level: 5 },
+        { skill: 'JavaScript', level: 5 }
+    ];
+    return memberSkills;
+}
